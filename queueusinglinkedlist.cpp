5b99c310e058dba96ba047454c1256f382ef2bc2
HN26 <plancksconstant01@gmail.com>
2022-07-24 13:59:09 +0530

    First Commit

---
+++ queueusinglinkedlist.cpp
@@ -0,0 +1,80 @@
+# include<iostream>
+
+using namespace std;
+
+struct Node 
+{
+    int data; 
+    struct Node *next;
+}*front = NULL, *rear = NULL;
+
+void enqueue ( int x)
+{
+    struct Node *t;
+    t = new struct Node;
+    if ( t==NULL)
+    {
+        cout<<"Queue is full"<<endl;
+    }
+    else
+    {
+        t->data = x;
+        t->next = NULL;
+        if(front == NULL)
+        {
+            front = rear = t;
+        }
+        else
+        {
+            rear->next = t;
+            rear = t;
+        }
+    }
+}
+
+int dequeue()
+{
+    int x = -1;
+    struct Node *t;
+    if(front == NULL)
+    {
+        cout<<"Queue is Empty"<<endl;
+    }
+    else
+    {
+        x = front->data;
+        t = front;
+        front = front->next;
+        delete t;
+    }
+    return x;
+}
+
+void Display()
+{
+    struct Node *p;
+    p = front;
+    while(p!=NULL)
+    {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+    
+    Display();
+
+    cout<<dequeue()<<endl;
+
+
+    return 0;
+}
